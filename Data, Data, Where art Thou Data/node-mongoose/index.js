3f7082449d6feddd1947f3831522a1c7420816bc
Joseff361 <joseff361@outlook.com>
2020-07-15 17:42:24 -0500

    Mongoose Part 3

--- Data, Data, Where art Thou Data/node-mongoose/index.js
+++ Data, Data, Where art Thou Data/node-mongoose/index.js
@@ -2,28 +2,40 @@ const mongoose = require('mongoose')
 const Dishes = require('./models/dishes')
 
 const url = 'mongodb://localhost:27017/conFusion'
-const connect = mongoose.connect(url)
+const connect = mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 connect.then((db) => {
     console.log('Connected correctly to server')
 
-    var newDish = Dishes({
-        name: 'Uthappiza',
+    Dishes.create({ //create and add
+        name: 'Uthappiza9',
         description: 'test'
     })
+    .then((dish) => {
+        console.log(dish)
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}},
+            {new: true} //Means that the new documente come back to us
+        ).exec()
+    })
+    .then((dish) => {
+        console.log(dish)
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish)
-            return Dishes.find({}).exec()
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         })
-        .then((dishes) => {
-            console.log(dishes)
-            return Dishes.remove({})
-        })
-        .then(() => {
-            return mongoose.connection.close()
-        })
-        .catch((err) => { console.log(err)})
+
+        return dish.save()
+    })
+    .then((dish) => {
+        console.log(dish)
+        return Dishes.deleteMany({})
+    })
+    .then(() => {
+        return mongoose.connection.close()
+    })
+    .catch((err) => { console.log(err)})
 })
 .catch((err) => { console.log(err)})
