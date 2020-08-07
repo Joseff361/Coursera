91cdfdc5f3c16e8af719faac97c094194b9256eb
Joseff361 <joseff361@outlook.com>
2020-08-06 20:33:01 -0500

    Passport Facebook

---
+++ Backend as a Service (BaaS)/OAuth/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '301799721056853',
+        clientSecret: 'eec15dfa8e169840ec93327b3f59e899'
+    }
+}
